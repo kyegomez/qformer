9c0078840235a1f10d1bf262c5a158a235650283
Eternal Reclaimer <98760976+kyegomez@users.noreply.github.com>
2023-12-29 14:11:42 -0500

    Update example.py

---
+++ example.py
@@ -0,0 +1,11 @@
+import torch
+from qformer import ImgBlock
+
+
+# 3d tensor, B x SEQLEN x DIM
+x = torch.randn(1, 32, 1024)
+image = torch.randn(1, 32, 1024)
+
+attn = ImgBlock(1024, 8, 1024)
+out = attn(x, image)
+print(out.shape)
